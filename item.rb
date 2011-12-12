27c0f0fe4bd15b2485fd3f36d1505b0d33750df8
Costi <cgavrilescu@enovafinancial.com>
2011-12-12 11:27:19 -0600

    Adds all attributes to item, adds item_spec

--- item.rb
+++ item.rb
@@ -1,4 +1,22 @@
+require 'date'
 class Item
-  attr_accessor :title, :status, :due_date
+  ATTRIBUTES = [:title, :status, :due_date, :pickup_by]
+  ATTRIBUTES.each do |attr|
+    attr_accessor attr 
+  end
+  
+  def initialize(attrs = {})
+    self.attributes = attrs
+  end
+
+  def attributes
+    hash = {}
+    ATTRIBUTES.each {|attr| hash[attr] = send(attr)}
+    hash
+  end
+
+  def attributes=(attrs)
+    ATTRIBUTES.each {|attribute_name| send("#{attribute_name}=", attrs[attribute_name])}
+  end
 
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -14,4 +14,28 @@ describe Item do
     item.due_date = '2011-12-12'
     item.due_date.should == '2011-12-12'
   end
+  it 'has a pickup_by date' do
+    item.pickup_by = '2011-12-12'
+    item.pickup_by.should == '2011-12-12'
+  end
+
+  describe 'attributes hash' do
+    ATTRS = Hash[:title => 'Title', :status => 'Status', :due_date => '2011-12-12', :pickup_by => '2011-12-29'] 
+    let(:attributes){ ATTRS }
+    let(:item){ described_class.new(ATTRS) } #scoping fun
+
+    it 'initializes with a hash of attributes' do
+      item.title.should == 'Title'
+      item.status.should == 'Status'
+      item.due_date.should == '2011-12-12'
+    end
+    it 'sets attributes using the attributes hash' do
+      new_attributes = attributes.merge(:title => 'New Title')
+      item.attributes = new_attributes
+      item.attributes.should == new_attributes
+    end
+    it 'gets attributes using the attributes hash' do
+      item.attributes.should == attributes
+    end
+  end
 end
