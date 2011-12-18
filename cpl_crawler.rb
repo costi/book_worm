85c025fc710dd6af65c83371f6a56480ef31f94f
Costi <cgavrilescu@enovafinancial.com>
2011-12-18 02:06:17 -0600

    Adds cpl_crawler tests

--- cpl_crawler.rb
+++ cpl_crawler.rb
@@ -1,18 +1,18 @@
 require 'mechanize'
 class CplCrawler
-  attr_reader :library_card, :zip_code
+  attr_reader :library_card, :zip_code, :http_agent
 
   def initialize(new_library_card, new_zip_code)
     @library_card = new_library_card
     @zip_code = new_zip_code
     @summary_page = nil
-    @agent = Mechanize.new { |agent|
+    @http_agent = Mechanize.new { |agent|
       agent.user_agent_alias = 'Mac Safari'
     }
   end
 
   def login_to_library
-    @agent.get('http://www.chipublib.org/mycpl/login/') do |login_page|
+    http_agent.get('http://www.chipublib.org/mycpl/login/') do |login_page|
       @homepage = login_page.form_with(:action => '/mycpl/login/') do |f|
         f.patronId = self.library_card
         f.zipCode = self.zip_code
@@ -38,9 +38,33 @@ class CplCrawler
   end
   
   private
-  def summary_page
-    @summary_page ||= @agent.click( @homepage.links.text(/Checked Out/) )
+
+  # Workflow:
+  # login_page -> fill login form -> click -> get home page => get summary page
+  
+  def login_page 
+    @login_page ||= http_agent.get('http://www.chipublib.org/mycpl/login/')
+  end
+
+  def login_form
+    @login_form ||= login_page.form_with(:action => '/mycpl/login/') 
+  end
+
+  def filled_in_login_form
+    login_form.patronId = self.library_card
+    login_form.zipCode = self.zip_code
+    login_form
+  end
+
+  def login!
+    @home_page = filled_in_login_form.click_button
+  end
+
+  def home_page
+    @home_page ||= login!
   end
   
-  
+  def summary_page
+    @summary_page ||= http_agent.click( home_page.links.text(/Checked Out/) )
+  end
 end
--- spec/cpl_crawler_spec.rb
+++ spec/cpl_crawler_spec.rb
@@ -7,15 +7,45 @@ describe CplCrawler do
 #    c.login_to_library.content.should include('My Library Card', 'My Personal Information', 'My Preferred Library')
 #  end
   
+  let(:parser){ parser = described_class.new('g1234', '60606')}
 
   it 'initializes with library card number and zip code' do
     parser = described_class.new('g1234', '60606')
     parser.zip_code.should == '60606'
     parser.library_card.should == 'g1234'
   end
+
+  it 'has an http_agent used to browse pages' do
+    parser.http_agent.should respond_to(:get)
+  end
+  
   describe 'Authentication:' do
-    it 'logs into library using card number and zip code'
-    it 'sets the homepage after logging in to the library'
+    let(:login_page){double 'Login Page', :form_with => login_form} 
+    let(:login_form){double 'Login Form', :click_button => :page_after_login,
+                                          :patronId= => parser.library_card,
+                                          :zipCode= => parser.zip_code}
+    before :each do
+      http_agent = double 'HTTP Agent', :get => login_page
+      parser.stub(:http_agent).and_return(http_agent)
+    end
+    it '1. retrieves the login page' do
+      parser.send(:login_page).should == login_page
+    end
+    it '2. finds the login form on the login page' do
+      parser.send(:login_form).should == login_form
+    end
+    it '3. fills in the form' do
+      login_form.should_receive(:patronId=).with(parser.library_card)
+      login_form.should_receive(:zipCode=).with(parser.zip_code)
+      parser.send(:filled_in_login_form).should == login_form
+    end
+    it '4. submits the form and gets the homepage for successful login' do
+      parser.send(:login!).should == :page_after_login
+    end
+
+    it 'sets the homepage after logging in to the library' do
+      parser.send(:home_page).should == :page_after_login
+    end
   end
   it 'can browse to summary page after reaching the homepage'
   it 'has the held items in the summary page'
