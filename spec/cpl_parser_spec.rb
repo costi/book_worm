31e6f78f74285a524131cc6da213bc4f84bd316b
Costi <cgavrilescu@enovafinancial.com>
2012-02-20 10:08:40 -0600

    Makes cpl_parser_spec use the constants in CplTest

--- spec/cpl_parser_spec.rb
+++ spec/cpl_parser_spec.rb
@@ -2,23 +2,24 @@
 require 'spec_helper'
 require 'book_worm/cpl_parser'
 require 'date'
+include BookWorm::CplTest
 
 describe BookWorm::CplParser do
   let(:fixture_path) { File.join(File.dirname(__FILE__), 'html_pages', 'cpl')}
   
   
   it "should parse fines" do
-    p = described_class.new(:all => File.read(File.join(fixture_path, "just_fines.html")))
+    p = described_class.new(:all => File.read(FINES_PAGE))
     p.total_fine_amount.should == 10 
   end
 
   it 'should get the correct number of held_items' do
-    p = described_class.new(:all => File.read(File.join(fixture_path, 'checked_out_overdue_on_hold.html')))
+    p = described_class.new(:all => File.read(SUMMARY_PAGE))
     p.held_items.size.should == 4
   end
 
   it 'should parse a held items row' do
-    p = described_class.new(:all => File.read(File.join(fixture_path, 'checked_out_overdue_on_hold.html')))
+    p = described_class.new(:all => File.read(SUMMARY_PAGE))
     held_item = p.held_items.first
     held_item[:title].should == 'Mon dernier soupir /'
     held_item[:status].should == 'Ready for pickup'
@@ -26,13 +27,13 @@ describe BookWorm::CplParser do
   end
  
   it 'should get the correct number of checked out items' do
-    p = described_class.new(:all => File.read(File.join(fixture_path, 'checked_out_overdue_on_hold.html')))
+    p = described_class.new(:all => File.read(SUMMARY_PAGE))
     p.checked_out_items.size.should == 5         
   end
 
   # checked out items don't include the overdue items in 
   it 'should parse a checked out items row' do
-    p = described_class.new(:all => File.read(File.join(fixture_path, 'checked_out_overdue_on_hold.html')))
+    p = described_class.new(:all => File.read(SUMMARY_PAGE))
     co_item = p.checked_out_items.first
     co_item[:title].should == 'Sexo para dummies /'
     co_item[:status].should == 'Checked out'
@@ -40,18 +41,18 @@ describe BookWorm::CplParser do
   end
   
   it 'should parse items with extra Spanish characters in it' do
-    p = described_class.new(:all => File.read(File.join(fixture_path, 'checked_out_overdue_on_hold.html')))
+    p = described_class.new(:all => File.read(SUMMARY_PAGE))
     p.checked_out_items[1][:title].should == 'El vendedor de sueños'
     p.checked_out_items[2][:title].should == 'Cómo iniciar su propio negocio /'
   end
 
   it 'should get the correct number of overdue items' do
-    p = described_class.new(:all => File.read(File.join(fixture_path, 'checked_out_overdue_on_hold.html')))
+    p = described_class.new(:all => File.read(SUMMARY_PAGE))
     p.overdue_items.size.should == 1
   end
 
   it 'should parse an overdue items row' do
-    p = described_class.new(:all => File.read(File.join(fixture_path, 'checked_out_overdue_on_hold.html')))
+    p = described_class.new(:all => File.read(SUMMARY_PAGE))
     co_item = p.overdue_items.first
     co_item[:title].should == "Cría cuervos" 
     co_item[:status].should == 'Overdue'
